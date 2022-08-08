beaf1785158bba7eb1679776e31aa24d469cdf37
Nick <ntowle@gmail.com>
2022-08-08 14:29:39 -0400

    Blackrock Mortar

--- ElitismHelper.lua
+++ ElitismHelper.lua
@@ -304,6 +304,7 @@ local Spells = {
 	-- Grimrail Depot
 	[164188] = 20,		-- Blackrock Bomb (Grimrail Bombardier)
 	[176131] = 20,		-- Cannon Barrage (Grom'kar Boomer)
+	[160963] = 20,		-- Blackrock Mortar (Grom'kar Boomer)
 	[166676] = 20,		-- Shrapnel Blast (Grom'kar Gunner)
 	[176147] = 20,		-- Ignite (Grom'kar Gunner)
 	[176033] = 20,		-- Flametongue (Grom'kar Cinderseer)
